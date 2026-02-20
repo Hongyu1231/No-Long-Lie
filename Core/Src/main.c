264d6cd6ae848b96c51439e26904e41437a46ead
unknown <185772174+Hongyu1231@users.noreply.github.com>
2026-02-20 16:32:36 +0800

    Renew the asm file to reduce 1 resister usage. Update the fall detection logic

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -10,6 +10,7 @@
 #include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_accelero.h"
 #include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_tsensor.h"
 #include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_gyro.h"
+#include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_psensor.h"
 
 #include "stdio.h"
 #include "string.h"
@@ -40,6 +41,7 @@ int main(void)
 	BSP_LED_Init(LED2);
 	BSP_ACCELERO_Init();
 	BSP_GYRO_Init();
+	BSP_PSENSOR_Init();
 
 	/*Set the initial LED state to off*/
 	BSP_LED_Off(LED2);
@@ -48,7 +50,7 @@ int main(void)
 	int accel_buff_y[4]={0};
 	int accel_buff_z[4]={0};
 	int i=0;
-	int delay_ms=1000; //change delay time to suit your code
+	int delay_ms=100; //change delay time to suit your code
 
 	while (1)
 	{
@@ -86,80 +88,191 @@ int main(void)
 
 
 		//Preprocessing the filtered outputs  The same needs to be done for the output from the assembly program as well
-		float accel_filt_c[3]={0};
-
-		accel_filt_c[0]=(float)mov_avg_C(N,accel_buff_x) * (9.8/1000.0f);
-		accel_filt_c[1]=(float)mov_avg_C(N,accel_buff_y) * (9.8/1000.0f);
-		accel_filt_c[2]=(float)mov_avg_C(N,accel_buff_z) * (9.8/1000.0f);
+//		float accel_filt_c[3]={0};
+//
+//		accel_filt_c[0]=(float)mov_avg_C(N,accel_buff_x) * (9.8/1000.0f);
+//		accel_filt_c[1]=(float)mov_avg_C(N,accel_buff_y) * (9.8/1000.0f);
+//		accel_filt_c[2]=(float)mov_avg_C(N,accel_buff_z) * (9.8/1000.0f);
 
 		/***************************UART transmission*******************************************/
 		char buffer[150]; // Create a buffer large enough to hold the text
 
 		/******Transmitting results of C execution over UART*********/
-		if(i>=3)
-		{
-			// 1. First printf() Equivalent
-			sprintf(buffer, "Results of C execution for filtered accelerometer readings:\r\n");
-			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
-
-			// 2. Second printf() (with Floats) Equivalent
-			// Note: Requires -u _printf_float to be enabled in Linker settings
-			sprintf(buffer, "Averaged X : %f; Averaged Y : %f; Averaged Z : %f;\r\n",
-					accel_filt_c[0], accel_filt_c[1], accel_filt_c[2]);
-			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+//		if(i>=3)
+//		{
+//			// 1. First printf() Equivalent
+//			sprintf(buffer, "Results of C execution for filtered accelerometer readings:\r\n");
+//			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+//
+//			// 2. Second printf() (with Floats) Equivalent
+//			// Note: Requires -u _printf_float to be enabled in Linker settings
+//			sprintf(buffer, "Averaged X : %f; Averaged Y : %f; Averaged Z : %f;\r\n",
+//					accel_filt_c[0], accel_filt_c[1], accel_filt_c[2]);
+//			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
 
 			/******Transmitting results of asm execution over UART*********/
 
-			// 1. First printf() Equivalent
-			sprintf(buffer, "Results of assembly execution for filtered accelerometer readings:\r\n");
-			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
-
-			// 2. Second printf() (with Floats) Equivalent
-			// Note: Requires -u _printf_float to be enabled in Linker settings
-			sprintf(buffer, "Averaged X : %f; Averaged Y : %f; Averaged Z : %f;\r\n",
-					accel_filt_asm[0], accel_filt_asm[1], accel_filt_asm[2]);
-			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+//			// 1. First printf() Equivalent
+//			sprintf(buffer, "Results of assembly execution for filtered accelerometer readings:\r\n");
+//			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+//
+//			// 2. Second printf() (with Floats) Equivalent
+//			// Note: Requires -u _printf_float to be enabled in Linker settings
+//			sprintf(buffer, "Averaged X : %f; Averaged Y : %f; Averaged Z : %f;\r\n",
+//					accel_filt_asm[0], accel_filt_asm[1], accel_filt_asm[2]);
+//			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
 
 			/******Transmitting Gyroscope readings over UART*********/
 
-			// 1. First printf() Equivalent
-			sprintf(buffer, "Gyroscope sensor readings:\r\n");
-			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
-
-			// 2. Second printf() (with Floats) Equivalent
-			// Note: Requires -u _printf_float to be enabled in Linker settings
-			sprintf(buffer, "Averaged X : %f; Averaged Y : %f; Averaged Z : %f;\r\n\n",
-					gyro_velocity[0], gyro_velocity[1], gyro_velocity[2]);
-			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
-		}
-
+//			// 1. First printf() Equivalent
+//			sprintf(buffer, "Gyroscope sensor readings:\r\n");
+//			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+//
+//			// 2. Second printf() (with Floats) Equivalent
+//			// Note: Requires -u _printf_float to be enabled in Linker settings
+//			sprintf(buffer, "Averaged X : %f; Averaged Y : %f; Averaged Z : %f;\r\n\n",
+//					gyro_velocity[0], gyro_velocity[1], gyro_velocity[2]);
+//			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+//		}
+//
 		HAL_Delay(delay_ms);	// 1 second delay
-
+//
 		i++;
 
 		// ********* Fall detection *********/
 		// write your program from here:
+		// 1. Calculate the squared magnitude of the acceleration (using filtered asm results)
+		float accel_sq = (accel_filt_asm[0] * accel_filt_asm[0]) + (accel_filt_asm[1] * accel_filt_asm[1]) + (accel_filt_asm[2] * accel_filt_asm[2]);
 
+		// Adjust the threshold
+//		sprintf(buffer, "Results of assembly execution for filtered accelerometer readings:\r\n");
+//		HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+//		sprintf(buffer, "accel_sq： %f\r\n", accel_sq);
+//		HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
 
+		// 2. Calculate squared magnitude of Gyroscope (angular velocity)
+	    float gyro_sq = (gyro_velocity[0] * gyro_velocity[0]) + (gyro_velocity[1] * gyro_velocity[1]) + (gyro_velocity[2] * gyro_velocity[2]);
+
+	    // Adjust the threshold
+//	    sprintf(buffer, "Gyroscope sensor readings:\r\n");
+//	    HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+//	    sprintf(buffer, "gyro_sq： %f\r\n", gyro_sq);
+//	    HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+
+		// 3. Get Barometer data
+		float current_pressure = BSP_PSENSOR_ReadPressure();
+
+	    // Adjust the threshold
+//	    sprintf(buffer, "Barometer sensor readings:\r\n");
+//	    HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+//	    sprintf(buffer, "current_pressure: %f\r\n", current_pressure);
+//	    HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+
+		// 4. State machine variables: 'static' retains their values across while-loop iterations
+		static uint8_t fall_state = 0;
+		static uint8_t timeout_timer = 0;
+		static uint8_t rotation_detected = 0;
+		static float baseline_pressure = 0.0f; // The pressure before falling down
+
+		// Physical characteristics thresholds for a fall (compared using squared values):
+		float ACCEL_FREEFALL_SQ = 20.0f; // Normal is 90
+		float GYRO_TUMBLE_SQ = 500000.0f; // Normal is 10**3
+		float PRESSURE_DROP_THRESHOLD = 0.09f;
+		float ACCEL_CATASTROPHIC_SQ = 400.0f;
+		float ACCEL_IMPACT_LOWER_SQ = 110.0f;
+		float ACCEL_IMPACT_HIGHER_SQ = 170.0f;
+
+		// fall detection logic
+		// Car crash or extreme violence detection
+		if (accel_sq > ACCEL_CATASTROPHIC_SQ)
+		{
+		    delay_ms = 50;
+		    sprintf(buffer, "!!! CATASTROPHIC IMPACT DETECTED !!! Acc:%.1f\r\n", accel_sq);
+		    HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+		    fall_state = 0; // Reset normal state machine
+		}
+
+		// NORMAL FALL LOGIC (Syncopal fall or Trip)
+		else if (accel_sq < ACCEL_FREEFALL_SQ) // Free-fall detected
+		{
+
+			// Debug
+		    sprintf(buffer, "Free-fall detected!!! accel_sq: %f\r\n", accel_sq);
+		    HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+
+			if (fall_state == 0)
+			{
+			    // Set the pressure baseline when free-fall just starts
+			    baseline_pressure = current_pressure;
+			}
+			fall_state = 1; // Phase 1 : free-fall detected
+			timeout_timer = 0; // Reset Timeout-Timer
+			rotation_detected = 0;
+		}
+
+		else if (fall_state == 1)
+			{
+				timeout_timer++;
+
+				// Lock the flag if a rapid tumble is detected at any point during the fall
+				if (gyro_sq > GYRO_TUMBLE_SQ)
+				{
+					rotation_detected = 1;
+					sprintf(buffer, "Rotation detected!!! gyro_sq: %f\r\n", gyro_sq);
+					HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+				}
+
+				if (accel_sq > ACCEL_IMPACT_LOWER_SQ && accel_sq < ACCEL_IMPACT_HIGHER_SQ)
+				{
+					// Phase 2: High impact detected
+					float pressure_diff = current_pressure - baseline_pressure;
+
+					// Debug
+				    sprintf(buffer, "Impact detected!!! accel_sq: %f\r\n", accel_sq);
+				    HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+
+					// Check for rotation OR altitude drop
+					if ((rotation_detected == 1 || gyro_sq > GYRO_TUMBLE_SQ) || (pressure_diff > PRESSURE_DROP_THRESHOLD))
+					{
+						delay_ms = 50;
+						sprintf(buffer, "!!! TRUE FALL !!! Acc:%.1f, dP:%.3f\r\n", accel_sq, pressure_diff);
+						HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+					}
+					fall_state = 0; // Reset state machine
+				}
+
+				else if (timeout_timer > 30)
+				{
+					// Debug
+				    sprintf(buffer, "Timeout detected!!!\r\n");
+				    HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+					// Timeout: Free-fall without subsequent impact (e.g., just lowering the board)
+					fall_state = 0; // Reset state
+				}
+			}
+
+		else
+		{
+			// Normal state: Change delay_ms to 100ms
+			delay_ms = 100;
+		}
 	}
-
-
 }
 
 
 
-int mov_avg_C(int N, int* accel_buff)
-{ 	// The implementation below is inefficient and meant only for verifying your results.
-	int result=0;
-	for(int i=0; i<N;i++)
-	{
-		result+=accel_buff[i];
-	}
-
-	result=result/4;
-
-	return result;
-}
+//int mov_avg_C(int N, int* accel_buff)
+//{ 	// The implementation below is inefficient and meant only for verifying your results.
+//	int result=0;
+//	for(int i=0; i<N;i++)
+//	{
+//		result+=accel_buff[i];
+//	}
+//
+//	result=result/4;
+//
+//	return result;
+//}
 
 static void UART1_Init(void)
 {
--- Core/Src/mov_avg.s
+++ Core/Src/mov_avg.s
@@ -16,13 +16,31 @@
  .align 2
 @ CG2028 Assignment, Sem 2, AY 2025/26
 @ (c) ECE NUS, 2025
-@ Write Student 1’s Name here: ABCD (A1234567R)
-@ Write Student 2’s Name here: WXYZ (A0000007X)
+@ Write Student 1’s Name here: Chen Hongyu (A0311920U)
+@ Write Student 2’s Name here: Hu Xiran (A0300137X)
 @ You could create a look-up table of registers here:
-@ R0 ...
-@ R1 ...
+@ R0: N (buffer size), counter
+@ R1: accel_buff (pointer to an int buffer containing the most recent samples)
+@ Return (R0): integer average of the N samples
+@ R2: Sum of the sample
+@ R3: Values of every samples
+@ R4: Total number of the samples, N (buffer size)
 @ write your program from here:
 mov_avg:
- PUSH {r2-r11, lr}
+ PUSH {r4, lr} @ save link register and R4
+ CMP r0, #0          @ Check if N less than 0
+ BLE end_early       @ If N <= 0, then end early
 
- POP {r2-r11, pc}
+ MOV r2, #0 @ initialise sum = 0
+ MOV r4, r0 @ Copy N to R4
+
+loop:
+ LDR r3, [r1], #4 @go to the memory address indicated by r1, and load the value at this address to r3; then go to the next address (increment 4 bytes)
+ ADD r2, r2, r3 @sum = sum + r3, add the value at r3 into sum
+ SUBS r0, r0, #1 @counter = counter - 1, at the same time set flag if r0 = 0
+ BNE loop @repeat
+
+ SDIV r0, r2, r4 @ R0 = total_sum / N (signed division)
+
+ end_early:
+  POP {r4, pc}
